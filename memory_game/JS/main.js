de72493d8dcfe436af5d352b190f2c918b0f025d
Svetla Syrimis <svetlasyrimis@Svetlas-Air.lan1>
2019-05-21 17:16:49 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,44 +1,48 @@
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardfour = "king";
+let cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
 
-let cards = ["queen", "queen", "king", "king"];
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 let cardsInPlay = [];
 
-// let cardOne = cards[0];
-// cardsInPlay.push(cardOne);
-// let cardTwo = cards[2];
-// cardsInPlay.push(cardTwo);
-
-// console.log("User flipped " + "the " +  cardOne + "!");
-// console.log("User flipped " + "the " + cardTwo + "!");
-
-// let cardOne = cards[0];
-// cardsInPlay.push(cardOne);
-// let cardTwo = cards[2];
-//     cardsInPlay.push(cardTwo);
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-      } else {
-        console.log("Sorry, try again.");
-      }
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
 }
 
 function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit)
+    cardsInPlay.push(cards[cardId].rank);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
-    // console.log("User flipped " + cards[cardId]);
 }
 
 flipCard(0);
 flipCard(2);
-
-
 
 
 
@@ -47,5 +51,4 @@ flipCard(2);
 // } else {
 //     alert("Sorry,try again.")
 // }
-// var cardThree = "king";
-// var cardfour = "king";
+
