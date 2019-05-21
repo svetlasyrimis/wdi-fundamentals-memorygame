079b136fcbe8f6483d8c99b713149820afa4273e
Svetla Syrimis <svetlasyrimis@Svetlas-Air.lan1>
2019-05-21 12:24:40 -0400

    create game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -6,18 +6,46 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+// let cardOne = cards[0];
+// cardsInPlay.push(cardOne);
+// let cardTwo = cards[2];
+// cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + "the " +  cardOne + "!");
-console.log("User flipped " + "the " + cardTwo + "!");
+// console.log("User flipped " + "the " +  cardOne + "!");
+// console.log("User flipped " + "the " + cardTwo + "!");
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match");
-} else {
-    alert("Sorry,try again.")
+// let cardOne = cards[0];
+// cardsInPlay.push(cardOne);
+// let cardTwo = cards[2];
+//     cardsInPlay.push(cardTwo);
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
 }
+
+function flipCard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+    // console.log("User flipped " + cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+// if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+//     alert("You found a match");
+// } else {
+//     alert("Sorry,try again.")
+// }
 // var cardThree = "king";
 // var cardfour = "king";
