ce6134cbff0e957e8b198cef0eaec4e0866910cf
Svetla Syrimis <svetlasyrimis@Svetlas-Air.lan1>
2019-05-21 11:32:37 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,7 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardfour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardfour = "king";
 
-console.log(`User flipped ${cardOne}`);
-console.log(`User flipped ${cardThree}`);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + "the " +  cardOne + "!");
+console.log("User flipped " + "the " + cardTwo + "!");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match");
+} else {
+    alert("Sorry,try again.")
+}
+// var cardThree = "king";
+// var cardfour = "king";
