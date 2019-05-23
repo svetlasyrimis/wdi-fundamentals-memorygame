709611a1a8b6c67ff8d1fd6870076174c6ac7421
Svetla Syrimis <svetlasyrimis@Svetlas-Air.lan1>
2019-05-22 23:12:27 -0400

    changes to game logic, "you won" message and automatic reset upon two win rounds

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -5,62 +5,88 @@ let cards = [
         rank: "queen",
         suit: "hearts",
         cardImage: "images/queen-of-hearts.png",
-        isRevealed: false
+        isRevealed: false,
+        cardElement: undefined
     },
     {
         rank: "queen",
         suit: "diamonds",
         cardImage: "images/queen-of-diamonds.png",
-        isRevealed: false
+        isRevealed: false,
+        cardElement: undefined
     },
     {
         rank: "king",
         suit: "hearts",
         cardImage: "images/king-of-hearts.png",
-        isRevealed: false
+        isRevealed: false,
+        cardElement: undefined
     },
     {
         rank: "king",
         suit: "diamonds",
         cardImage: "images/king-of-diamonds.png",
-        isRevealed: false
+        isRevealed: false,
+        cardElement: undefined
     }
 ];
 
 let cardsInPlay = [];
-
+const backImageURL = "images/back.png";
 function createBoard() {
     for (var i = 0; i < cards.length; i++) {
         var cardElement = document.createElement('img');
-        cardElement.setAttribute('src',"images/back.png");
+        cardElement.setAttribute('src', backImageURL);
         cardElement.setAttribute('data-id', i);
-        cardElement.addEventListener('click',flipCard);
-        document.getElementById('game-board').appendChild(cardElement); 
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+        cards[i].cardElement = cardElement;
     }
-    
+
 }
 
 function flipCard() {
     const cardId = this.getAttribute('data-id');
+    let currentCard = cards[cardId];
+    if (!currentCard.isRevealed) {
+        this.setAttribute('src', currentCard.cardImage);
+        currentCard.isRevealed = true;
 
-    cardsInPlay.push(cards[cardId].rank);
-    
-    this.setAttribute('src',cards[cardId].cardImage);
-    console.log(cardsInPlay);
-    if (cardsInPlay.length % 2 === 0) {
-        checkForMatch();
+        cardsInPlay.push(currentCard);
+
+        console.log(cardsInPlay);
+        if (cardsInPlay.length % 2 === 0) {
+
+            setTimeout(checkForMatch, 300);
+        }
     }
+
+
+
 }
 
 function checkForMatch() {
-    if (cardsInPlay[cardsInPlay.length-2] === cardsInPlay[cardsInPlay.length-1]) {
-        setTimeout(function(){
+    let cardA = cardsInPlay[cardsInPlay.length - 2];
+    let cardB = cardsInPlay[cardsInPlay.length - 1];
+    if (cardA.rank === cardB.rank) {
+        if (cards.length === cardsInPlay.length) {
+            alert("Congratulations! You won the game. Click OK to reset.");
+            for (let i = 0; i < cards.length; i++) {
+                cards[i].cardElement.setAttribute('src', backImageURL);
+                cards[i].isRevealed = false;
+                cardsInPlay.pop();
+            }
+        } else {
             alert("You found a match!");
-        },300);
+        }
     } else {
-        setTimeout(function() {
-            alert("Sorry, try again.");
-        },300);
+        cardA.isRevealed = false;
+        cardA.cardElement.setAttribute('src', backImageURL);
+        cardB.isRevealed = false;
+        cardB.cardElement.setAttribute('src', backImageURL);
+        cardsInPlay.pop();
+        cardsInPlay.pop();
+        alert("Sorry, try again.");
     }
 
 }
